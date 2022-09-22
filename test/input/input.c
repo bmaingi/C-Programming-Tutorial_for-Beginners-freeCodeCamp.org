bb2f5f0d7d8f3ecf86a4511b5cf82f593065ed81
mwirigi <bmmaingi17@gmail.com>
2022-09-22 12:25:07 +0300

    input

---
+++ test/input/input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char firstname[20], lasname[20];
+
+	printf("Enter youre first name : ");
+	scanf("%s", firstname);
+
+	printf("Enter youre last name :");
+	scanf("%s", lastname);
+
+	printf("youre Full name is %s %s", firstname, lastname);
+
+	return 0;
+}
