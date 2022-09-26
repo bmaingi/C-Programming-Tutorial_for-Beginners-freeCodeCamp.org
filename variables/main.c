7ce364afe8d3c78fb742ff706896e1adfec976ac
mwirigi <bmmaingi17@gmail.com>
2022-09-26 23:38:15 +0300

    variables

--- variables/main.c
+++ variables/main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("There once was a man named George\n");
+    printf("he was 70 years old.\n");
+    printf("he really liked the name George\n");
+    printf("but did not likr being 70\n");
+
     return 0;
 }
