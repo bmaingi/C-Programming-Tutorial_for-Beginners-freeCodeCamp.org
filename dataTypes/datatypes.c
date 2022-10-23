0a94e9ef0c298c6f0e1b07e583678cb34f56d2fa
mwirigi <bmmaingi17@gmail.com>
2022-10-23 23:41:44 +0300

    datatypes

--- dataTypes/datatypes.c
+++ dataTypes/datatypes.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>:
+#include <stdlib.h>
+
+int main()
+{
+    // data type to store in container
+    // numbers int and double plus float
+
+    int age = 40;
+    double weight = 44.6;
+    double gpa = 4.0;
+
+    // single characters
+    char grade = 'A';
+
+    // string of characters
+    char password[] = "Software Development"; // array
+    return 0;
+}
